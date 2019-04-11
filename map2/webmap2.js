cd67a37e6adb50f4a7582f88d14873bff572a94d
AlyssaStewart <48488170+AlyssaStewart@users.noreply.github.com>
2019-04-11 15:57:05 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,13 +1,7 @@
 let mymap = L.map('mymap1').setView([35.17, -98.25], 5)
-var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(map1), {
-	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-let mymap = L.marker([32, -85]).addTo(mymap)
-let mymap = L. polygon([
-    [50.517, -0.05],
-    [50.512, -0.03],
-    [50.52, -0.064]
-]).addTo(mymap);
-mymap.bindPopup('My site polygon')
-mymap.bindPopup('My site marker')
+var OpenStreetMap_DE = L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(mymap)
