52bff5f89ca1e0ae4938e022713ff682cc0a8437
AlyssaStewart <48488170+AlyssaStewart@users.noreply.github.com>
2019-05-03 16:34:05 -0500

    Create webfinalmap.js

---
+++ finalmap/webfinalmap.js
@@ -0,0 +1,4 @@
+let mymap = L.map('map1').setView([35.15, -99], 4)
+var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+}). addTo(mymap)
