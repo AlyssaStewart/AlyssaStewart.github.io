324b64ca2fec846aa31fda91c88d692ca36dab24
AlyssaStewart <48488170+AlyssaStewart@users.noreply.github.com>
2019-05-03 15:49:18 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,4 +1,4 @@
-let mymap = L.map('mymap1').setView([35.17, -98.25], 5)
+let mymap = L.map('map1').setView([35.17, -98.25], 5)
 var OpenStreetMap_DE = L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
