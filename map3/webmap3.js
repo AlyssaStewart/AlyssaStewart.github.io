4afa1efb651114aad8884691c3e590ad8647ebbb
AlyssaStewart <48488170+AlyssaStewart@users.noreply.github.com>
2019-05-03 15:56:11 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,13 +1,7 @@
 let mymap = L.map('map1').setView([35.17, -98.25], 5)
-var OpenStreetMap_DE = L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap)
-var OpenStreetMap_DE = L.tilelayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-  layers: 'warnings_c',
-  format: 'image/png',
-  transparent: true
-  attribution: 'NOAA, Iowa State University'
-}).addTo(mymap)
+
+
