3879802e051dd6b6b5b4d97eae336168e7e0249b
AlyssaStewart <48488170+AlyssaStewart@users.noreply.github.com>
2019-05-03 15:45:41 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,1 +1,13 @@
-
+let mymap = L.map('mymap1').setView([35.17, -98.25], 5)
+var OpenStreetMap_DE = L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(mymap)
+var OpenStreetMap_DE = L.tilelayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnings_c',
+  format: 'image/png',
+  transparent: true
+  attribution: 'NOAA, Iowa State University'
+}).addTo(mymap)
