80e5858994686876941deb466073a8990648ea7e
AlyssaStewart <48488170+AlyssaStewart@users.noreply.github.com>
2019-05-03 16:50:11 -0500

    Update webfinalmap.js

--- finalmap/webfinalmap.js
+++ finalmap/webfinalmap.js
@@ -1,4 +1,18 @@
 let mymap = L.map('map1').setView([35.15, -99], 4)
-var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-}). addTo(mymap)
+var Thunderforest_OpenCycleMap = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: '<your apikey>',
+	maxZoom: 22
+}).addTo(mymap)
+let foodTrucksUrl = 'https://opendata.arcgis.com/datasets/33f825353da04a5fbb2c204a6c6128c7_0.geojson'
+jQuery.getJSON(foodTrucksUrl, function (data) {
+	L.geoJSON(data).addTo(foodMap)
+})
+L.geoJSON (data, {style: {color: 'red' } }).addTo(foodMap)
+let foodStyle = {
+	color: 'red',
+	weight: 1,
+	fillOpacity: 0.2
+}
+let StateGeojsonOptions = { style: foodStyle }
+L.geogJSON(data, foodGeojsonOptions).addTo(foodMap)
